9443dfdba8bb8ad0025dbe4667898c8e7d69d376
Conor-Fleming <conorfleming03@gmail.com>
2022-12-23 12:30:18 -0800

    improving error handling and refactor with wait groups and go routines

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
+	"sync"
 )
 
 type NameResponse struct {
@@ -24,8 +26,31 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// Get first and last names from getNameData() and use those as args in getJokeData()
 		// write the returned joke to the response
-		first, last := getNameData()
-		w.Write([]byte(getJokeData(first, last)))
+		var wg sync.WaitGroup
+		wg.Add(2)
+
+		var name *NameResponse
+		var joke *JokeResponse
+
+		//unsure about error handling in this case
+		//printed message to log and return nil upon error in these two functions so i suppose the error is handled if code falls through on line 47
+		go func() {
+			name, _ = getNameData()
+			wg.Done()
+		}()
+		go func() {
+			joke, _ = getJokeData()
+			wg.Done()
+		}()
+		wg.Wait()
+
+		if name != nil && joke != nil {
+			joke.Value.Joke = strings.Replace(joke.Value.Joke, "--first--", name.FirstName, 1)
+			joke.Value.Joke = strings.Replace(joke.Value.Joke, "--last--", name.LastName, 1)
+
+			w.Write([]byte(joke.Value.Joke))
+		}
+		http.Error(w, "There was an error getting the data", http.StatusInternalServerError)
 	})
 
 	// Create the http server at localhost:8080
@@ -38,38 +63,42 @@ func main() {
 	server.ListenAndServe()
 }
 
-func getNameData() (string, string) {
+func getNameData() (*NameResponse, error) {
 	//Calling Api for the random name
 	nameResp, err := http.Get("https://names.mcquay.me/api/v0/")
 	if err != nil {
-		fmt.Println("Getting name api response: ", err)
+		log.Print("Getting name api response: ", err)
+		return nil, err
 	}
 	defer nameResp.Body.Close()
 
 	//reading response to byte slice
 	nameRespData, err := ioutil.ReadAll(nameResp.Body)
 	if err != nil {
-		log.Fatal("Read Response Data: ", err)
+		log.Print("Read Response Data: ", err)
+		return nil, err
 	}
 
 	// Unmarshalling JSON to name variable of type NameResponse (declared above)
-	var name NameResponse
+	name := &NameResponse{}
 	err = json.Unmarshal(nameRespData, &name)
 	if err != nil {
 		log.Print("Unmarshal Name: ", err)
+		return nil, err
 	}
 
-	return name.FirstName, name.LastName
+	return name, nil
 }
 
-func getJokeData(first, last string) string {
+func getJokeData() (*JokeResponse, error) {
 	//formatting url with given first and last name
-	jokeURL := fmt.Sprintf("http://joke.loc8u.com:8888/joke?limitTo=nerdy&firstName=%s&lastName=%s", first, last)
+	jokeURL := fmt.Sprintf("http://joke.loc8u.com:8888/joke?limitTo=nerdy&firstName=--first--&lastName=--last--")
 
 	//Calling Api for the random joke
 	jokeResp, err := http.Get(jokeURL)
 	if err != nil {
 		log.Print("Getting joke api response: ", err)
+		return nil, err
 	}
 	defer jokeResp.Body.Close()
 
@@ -77,16 +106,16 @@ func getJokeData(first, last string) string {
 	jokeRespData, err := ioutil.ReadAll(jokeResp.Body)
 	if err != nil {
 		log.Print("Read Response Data: ", err)
+		return nil, err
 	}
 
 	//Unmarshalling JSON to joke variable of type JokeResponse
-	var joke JokeResponse
+	joke := &JokeResponse{}
 	err = json.Unmarshal(jokeRespData, &joke)
 	if err != nil {
 		log.Print("Unmarshal Joke: ", err)
+		return nil, err
 	}
 
-	output := joke.Value.Joke + "\n"
-
-	return output
+	return joke, nil
 }

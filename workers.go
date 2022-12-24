41e0c7858d296f06257d67440713ce23bb7164d2
Conor-Fleming <conorfleming03@gmail.com>
2022-12-24 15:43:12 -0800

    messing with channels

--- workers.go
+++ workers.go
@@ -2,19 +2,20 @@ package main
 
 import (
 	"errors"
+	"log"
 	"strings"
-	"sync"
 )
 
 func startWorkers() (string, error) {
-	var wg sync.WaitGroup
+	//Using wait groups
+	/*var wg sync.WaitGroup
 	wg.Add(2)
 
 	var name *NameResponse
 	var joke *JokeResponse
 
 	//unsure about error handling in this case
-	//printed message to log and return nil upon error in these two functions so i suppose the error is handled if code falls through on line 47
+	//printed message to log and return nil upon error in these two functions so i suppose the error is handled if code falls through on line 28
 	go func() {
 		name, _ = NameData()
 		wg.Done()
@@ -23,8 +24,29 @@ func startWorkers() (string, error) {
 		joke, _ = JokeData()
 		wg.Done()
 	}()
-	wg.Wait()
+	wg.Wait()*/
 
+	nameChan := make(chan *NameResponse)
+	jokeChan := make(chan *JokeResponse)
+
+	go func() {
+		name, err := NameData()
+		if err != nil {
+			log.Println(err)
+		}
+		nameChan <- name
+	}()
+	go func() {
+		joke, err := JokeData()
+		if err != nil {
+			log.Println(err)
+		}
+		jokeChan <- joke
+	}()
+	name := <-nameChan
+	joke := <-jokeChan
+
+	//doing it this way so the JokeData() func doesnt have to wait for the NameData values before we call it.
 	if name != nil && joke != nil {
 		// Replace name values in Joke with values from Name API
 		joke.Value.Joke = strings.ReplaceAll(joke.Value.Joke, "*first", name.FirstName)
